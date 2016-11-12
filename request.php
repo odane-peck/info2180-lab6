3772347f6c7282ca92b189d7e396c76dca38cd52
Odane E Peck <odane.peck@mymona.uwi.edu>
2016-11-12 02:36:15 +0000

    Updating files

--- request.php
+++ request.php
@@ -1,7 +1,11 @@
 <?php
+
 // accept a term (keyword)
 // respond with a value
+
 $query = $_GET['q'];
+$all = $_GET['all'];
+
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -11,5 +15,24 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?> 
+    <entries> 
+        <definition name="definition" author="John"> 
+            a statement of the exact meaning of a word, especially in a dictionary. 
+        </definition> 
+        <definition name="bar" author="mary"> 
+            a place that sells alcholic beverages 
+        </definition> 
+        <definition name="ajax" author="Kimberly"> 
+            technique which involves the use of javascript and xml 
+        </definition> 
+</entries>';
+
+if($all){
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+}else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 // JavaScript File
 window.onload = function()
 {
+    var results = document.getElementById("result")
+    var httpRequest;
+    var searchBtn = document.getElementById("searchBtn");
+    var all_btn = document.getElementById("all");
+    all_btn.addEventListener("click", function(e) {
+		e.preventDefault();
+		httpRequest = new XMLHttpRequest();
+		var list = document.createElement('ul');
+		results.innerHTML = "";
+		list.setAttribute("id","list");
+		results.appendChild(list);
+		httpRequest.onreadystatechange = getAllData;
+		var url = "request.php?q=&all=true";
+		httpRequest.open("GET", url);
+		httpRequest.send();
+		function getAllData() {
+			if(httpRequest.readyState === XMLHttpRequest.DONE){
+				if(httpRequest.status == 200){
+					var xmlResponse = httpRequest.responseXML;
+					var definition = xmlResponse.getElementsByTagName("definition");
+					for (var x = 0; x < definition.length; x++) {
+			        	var h3 = document.createElement("H3");
+			        	var p = document.createElement("P");
+			        	var p_author = document.createElement("P");
+			            var def = document.createTextNode(definition[x].innerHTML);
+			            var author = document.createTextNode(definition[x].getAttribute("author"));
+			            var name = document.createTextNode(definition[x].getAttribute("name"));
+			            h3.appendChild(name);
+			            p.appendChild(def);
+			            p_author.appendChild(author);
+			        	results.appendChild(h3);
+			        	results.appendChild(p);
+			        	results.appendChild(p_author);
+			        }
+				}
+			}
+		}
+	});
 
-  
-  var searchBtn = document.getElementById("searchBtn");
 
   searchBtn.addEventListener("click", function(element) {
     element.preventDefault();
 
-    var httpRequest = new XMLHttpRequest();
+    httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = QuoteL;
     var url = "request.php?q="+document.getElementById("p").value;
     httpRequest.open("GET", url);
